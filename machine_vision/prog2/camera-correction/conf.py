fce40983471ab0d90af6b8ee120d09ad22d75fe4
ulong2 <shlchen@whu.edu.cn>
2023-11-04 22:09:18 +0800

    update

---
+++ machine_vision/prog2/camera-correction/conf.py
@@ -0,0 +1,19 @@
+#########
+# FOR YOUR OWN CALIBRATION
+
+CAL_START_COEFF = 0.00000001
+CAL_COEFF_INC_FACTOR = 0.0000007
+CAL_EPOCH_NUMB = 12
+
+
+IMAGE_PATH = './images/test4.jpg'
+OUTPUT_PATH = './results/test4.jpg'
+
+
+# chessboard
+# CAL_START_COEFF = 0.000002
+# CAL_COEFF_INC_FACTOR = 0.000002
+# CAL_EPOCH_NUMB = 4
+
+# IMAGE_PATH = './images/board2.jpg'
+# OUTPUT_PATH = './results/board2.jpg'
